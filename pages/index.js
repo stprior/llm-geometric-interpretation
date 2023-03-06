7b19dbaf458948f1ab30ab9c0a681d501cb92fa0
Stephen Prior <stephen@prior-technology.ie>
2023-03-06 00:03:43 +0000

    outline topics and get gl rendering started

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,57 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Box from '@components/WebGlApp'
+import { Canvas, useFrame } from '@react-three/fiber'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Geometric Interpretation of Large Language Models</title>
         <link rel="icon" href="/favicon.ico" />
+        
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="WebGL Test" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          This is where I explore my intuitions about large language models and word embeddings, and try to express them more 
+          clearly through geometric visualizations.
         </p>
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
+
+        <h2>From arrays to vector spaces</h2>
+        <p>
+          Any program or block of written text can be represented as a sequence of numbers. For the numbers to make sense as a vector
+          space they need to be organised in such a way that we can combine them and perform operations that make sense.
+        </p>
+
+        <h2>
+          Word Embeddings
+        </h2>
+
+        <h2>
+          Neural Networks
+        </h2>
+
+        <h2>
+          Transformers
+        </h2>
+
+        <h2>
+          Human Language
+        </h2>
+        <h2>
+          Language models
+        </h2>
+          
       </main>
 
-      <Footer />
     </div>
   )
 }
